626e232ffaaf2c3edccaa56e3dd407d4ab690f03
shahid <shahid.ice.nstu@gmail.com>
2022-03-16 14:59:36 +0600

    complete todo app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+function Item(props) {
+  const items = props.items;
+  const result = items.map((item, index) => {
+    return (
+      <div key={index} className="item">
+        <p>{item}</p>
+        <p className="remove_item" onClick={() => props.removeItem(index)}>
+          X
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    );
+  });
+  return result;
+}
+
+function ItemDisplay(props) {
+  return (
+    <div>
+      <Item items={props.items} removeItem={props.removeItem} />
     </div>
   );
+}
+
+class Form extends React.Component {
+  state = {
+    item: "",
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    // console.log("handleChange", name, value);
+    this.setState({ [name]: value });
+  };
+  handleSubmit = () => {
+    this.props.addItem(this.state);
+    this.setState({ item: "" });
+  };
+
+  render() {
+    const item = this.state.item;
+    return (
+      <form>
+        <label htmlFor="item">Add Item </label>
+        <input
+          type="text"
+          name="item"
+          id="item"
+          value={item}
+          onChange={this.handleChange}
+        />
+        <input type="button" value="Submit" onClick={this.handleSubmit} />
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    items: ["python", "Ruby", "Rust"],
+  };
+
+  addItem = (state) => {
+    const items = this.state.items.slice();
+    items.push(state.item);
+    this.setState({ items: items });
+  };
+  removeItem = (remove_item_index) => {
+    const filter_items = this.state.items.filter((item, index) => {
+      return index != remove_item_index;
+    });
+    this.setState({ items: filter_items });
+  };
+
+  render() {
+    return (
+      <div>
+        <ItemDisplay items={this.state.items} removeItem={this.removeItem} />
+        <Form addItem={this.addItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
